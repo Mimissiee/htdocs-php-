b385a91ed3c7e16e2ac948236845b7ab0bc66fec
Mimissiee <growfitzgt@gmail.com>
2025-04-29 14:13:26 +0800

    soal elseif

--- soal 7(quiz).php
+++ soal 7(quiz).php
@@ -3,6 +3,8 @@
 $bukhori_sehat = true;
 $motor_nyala = false;
 
+// Kondisi Bukhori tidak berangkat karana mogok
+
 if ($bukhori_sehat && $motor_nyala) {
     echo "Bukhori berangkat.";
 } elseif ($bukhori_sehat && !$motor_nyala) {
