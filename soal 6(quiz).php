ce0d423f352ce4390d4b2e187e698dc1eb959c89
Mimissiee <growfitzgt@gmail.com>
2025-04-29 13:49:59 +0800

    soal elseif

---
+++ soal 6(quiz).php
@@ -0,0 +1,14 @@
+<?php
+
+$nilai = 94; 
+
+if ($nilai >= 90) {
+    echo "Sangat mampu";
+} elseif ($nilai >= 80) {
+    echo "Baik";
+} elseif ($nilai >= 75) {
+    echo "Cukup";
+} else {
+    echo "Tidak Mampu";
+}
+
